a478750cedcf159259849dbec0fd076fd01b0d33
HuZz-DeV <huzzderprogrammer@gmail.com>
2020-10-03 13:03:27 +0300

    spelling mistake

--- JS-ES6/7_destructing.js
+++ JS-ES6/7_destructing.js
@@ -1,12 +1,12 @@
 // ES5
 const person1 = {
-    firstname: "Mohammad",
-    lastname: "Agha",
-    age: 21
+  firstname: "Mohammad",
+  lastname: "Agha",
+  age: 21,
 };
 
 // const fname = person1.firstname;
-// const lname = person1.lname;
+// const lname = person1.lastname;
 // const age = person1.age;
 
 // ES6
@@ -21,18 +21,20 @@ console.log(firstname, lastname, personAge);
 // For nested objects
 
 const person2 = {
-    firstname: "Mohammad",
-    lastname: "Agha",
-    age: 21,
-    address: {
-        city: "Aramoun",
-        street: "Doha",
-        building: "Shorouk",
-        floor: 4
-    }
-}
+  firstname: "Mohammad",
+  lastname: "Agha",
+  age: 21,
+  address: {
+    city: "Aramoun",
+    street: "Doha",
+    building: "Shorouk",
+    floor: 4,
+  },
+};
 
-const { address: { city, street, building, floor } } = person2;
+const {
+  address: { city, street, building, floor },
+} = person2;
 console.log(city, street, building, floor);
 
 // For arrays
@@ -44,7 +46,7 @@ console.log(a, b);
 const [, , , c, d] = arr;
 console.log(c, d);
 
-const [, e, , f,] = arr;
+const [, e, , f] = arr;
 console.log(e, f);
 
 // If we want to swap two numbers
@@ -54,7 +56,6 @@ console.log(num1, num2);
 [num1, num2] = [num2, num1];
 console.log(num1, num2);
 
-
 // Destructing with rest parameter to reassign array elements
 
 const array1 = [1, 2, 3, 4, 5, 6, 7];
@@ -63,14 +64,14 @@ console.log(x, y, newArr);
 
 // Destructing to pass an object as a function's parameter
 
-const printInfo = data => {
-    const {
-        firstname: fname,
-        lastname: lname,
-        age: personAge,
-        address: { city, street, building, floor: floorNumber }
-    } = person2;
+const printInfo = (data) => {
+  const {
+    firstname: fname,
+    lastname: lname,
+    age: personAge,
+    address: { city, street, building, floor: floorNumber },
+  } = person2;
 
-    console.log(fname, lname, personAge, city, street, building, floorNumber);
-}
+  console.log(fname, lname, personAge, city, street, building, floorNumber);
+};
 printInfo(person2);
